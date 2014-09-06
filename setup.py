56c557c7edb9cd691dfb8673f59258584b2a53a9
astex <0astex@gmail.com>
2014-09-06 00:42:23 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name="needs",
+    packages=["needs"],
+    version="1.0.0",
+    description="Boolean Contexts",
+    author="Phil Condreay",
+    author_email="0astex@gmail.com",
+    url="https://github.com/astex/needs",
+    keywords=["context", "permissions", "needs", "roles"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet"
+        ],
+    long_description="""\
+Needs
+-----
+Boolean contexts.  A pythonic way of expressing what your code needs
+"""
+)
