ab19a8e6f144f097aca97e71f586d44a187c4376
astex <0astex@gmail.com>
2014-10-20 13:47:18 -0400

    update meta for new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="needs",
     packages=["needs"],
-    version="1.0.4",
+    version="1.0.5",
     description="Boolean Contexts",
     author="Phil Condreay",
     author_email="0astex@gmail.com",
